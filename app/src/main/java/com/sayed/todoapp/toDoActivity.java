4e864265106cf70ff27e1075211ac616cbe3edb8
Sayed ElAbady <sayed.elabady95@gmail.com>
2017-05-18 19:52:30 +0200

    toDo Activities work

--- app/src/main/java/com/sayed/todoapp/toDoActivity.java
+++ app/src/main/java/com/sayed/todoapp/toDoActivity.java
@@ -1,13 +1,120 @@
 package com.sayed.todoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class toDoActivity extends AppCompatActivity {
+    RecycleAdapter adapter;
+    ArrayList<todo> todoList;
+    public void retrieveDataFromDB(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        database.getReference("todoList").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        todoList.clear();
+                        for (DataSnapshot data : dataSnapshot.getChildren()) {
+                            todo toDo = data.getValue(todo.class);
+                            todoList.add(toDo);
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        retrieveDataFromDB();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_to_do);
+        setContentView(R.layout.activity_to_do);
+        todoList = new ArrayList<>();
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        adapter = new RecycleAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
     }
+
+    public void enterEditActivity(View view) {
+        Intent intent = new Intent(toDoActivity.this, toDoEditActivity.class);
+        startActivity(intent);
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+       retrieveDataFromDB();
+    }
+
+    private class RecycleAdapter extends RecyclerView.Adapter {
+
+
+        @Override
+        public int getItemCount() {
+            return todoList.size();
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_item, parent, false);
+            SimpleItemViewHolder pvh = new SimpleItemViewHolder(v);
+            return pvh;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            SimpleItemViewHolder viewHolder = (SimpleItemViewHolder) holder;
+            viewHolder.position = position;
+            todo todo = todoList.get(position);
+            ((SimpleItemViewHolder) holder).title.setText(todo.getName());
+        }
+
+        public final class SimpleItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView title;
+            public int position;
+
+            public SimpleItemViewHolder(View itemView) {
+                super(itemView);
+                itemView.setOnClickListener(this);
+                title = (TextView) itemView.findViewById(R.id.myTextView);
+            }
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        }
+    }
+
 }
